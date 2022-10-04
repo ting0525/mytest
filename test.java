5239c00abbf1d009cbe13438d60deb4be1d23d3e
ting0525 <zg45154551@gmail.com>
2022-10-04 15:21:15 +0800

    test

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+
+    public static void main(String[] args) {
+        int number1 = 10;
+        float number2 = 10.6f;
+        float result1 = number1 + number2;
+        System.out.println(result1);
+    }
+}
